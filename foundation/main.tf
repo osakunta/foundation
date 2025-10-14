b1243debdcd35d693fdad4eb0bb95599ad2ae423
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2025-10-14 18:32:42 +0000

    update

--- foundation/main.tf
+++ foundation/main.tf
@@ -7,3 +7,10 @@ provider "google" {
 data "google_project" "foundation_project" {
   project_id = var.gcp_project_id
 }
+
+resource "google_project_service" "services" {
+  for_each = toset(["cloudbilling.googleapis.com"])
+
+  project = var.gcp_project_id
+  service = each.key
+}
--- foundation/osakunta.tf
+++ foundation/osakunta.tf
@@ -2,16 +2,3 @@ resource "google_folder" "osakunta_folder" {
   display_name = "osakunta"
   parent       = "organizations/${data.google_project.foundation_project.org_id}"
 }
-
-moved {
-  from = google_folder.osakunta-folder
-  to   = google_folder.osakunta_folder
-}
-moved {
-  from = google_project.osakunta-website-prod
-  to   = google_project.osakunta_website_prod
-}
-moved {
-  from = google_project_service.osakunta-website-prod-services
-  to   = google_project_service.osakunta_website_prod_services
-}
